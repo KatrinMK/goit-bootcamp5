a1f4d0153aec4b6903f9dfea6c5d5e5b53537d00
Katrin <katringm03@gmail.com>
2018-04-11 10:43:38 +0300

    add module-1

---
+++ home work/module-1/js/javascript.js
@@ -0,0 +1,43 @@
+ const one = 'qwertyuiop[]';
+ console.log(one);
+
+ const two = 'asdfghjkl;';
+ console.log(two);
+
+ const three = 'zxcvbnm,./';
+ console.log(three);
+
+ let a = (one.length);
+ console.log(a);
+
+ let b = (two.length);
+ console.log(b);
+
+ let c = (three.length);
+ console.log(c);
+
+ let d = (one.charAt(0))
+ console.log(d);
+
+ let e = (two.charAt(0));
+ console.log(e);
+
+ let f = (three.charAt(0));
+ console.log(f);
+
+ let g = (one[one.length-1]);
+ console.log(g);
+
+ let k = (two[two.length-1]);
+ console.log(k);
+
+ let l = (three[three.length-1]);
+ console.log(l);
+
+ let m = (one.indexOf('['));
+ console.log(m);
+
+ let n = (one.indexOf(']'));
+ console.log(n);
+
+ console.log(`first string ${one}, second string ${two}, third string ${three}, four string ${a}, fifth string ${b}, sixth string ${c}, seventh string ${d}, eigth string ${e}, ninth string ${f}, tenth string ${g}, eleventh string ${k}, twelfth string ${l}, thirteenth string ${m}, fourteenth string ${n}`);
